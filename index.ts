46a2e6c7f717427e9277b8267a5ca6bdec573790
glebbash <glebbash@gmail.com>
2026-03-25 23:34:13 +0000

    feat: add proxy endpoint

--- index.ts
+++ index.ts
@@ -3,11 +3,38 @@ import { Hono } from "hono";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { serve } from "@hono/node-server";
 
-const app = new Hono();
-
 const foldersWithIndexHtml = readdirSync("static", { withFileTypes: true })
   .filter((d) => d.isDirectory())
   .map((d) => d.name);
+
+const app = new Hono();
+
+app.all("/proxy/*", async (ctx) => {
+  const targetUrl = ctx.req.path.replace(/^\/proxy\//, "");
+  if (!targetUrl) {
+    return ctx.text("Target URL is required", 400);
+  }
+
+  try {
+    console.log(`Proxying request to: [${ctx.req.method}] ${targetUrl}`);
+
+    const response = await fetch(targetUrl, {
+      method: ctx.req.method,
+      headers: ctx.req.header(),
+      body: ["GET", "HEAD"].includes(ctx.req.method)
+        ? undefined
+        : await ctx.req.arrayBuffer(),
+    });
+
+    return new Response(response.body, {
+      status: response.status,
+      headers: response.headers,
+    });
+  } catch (error) {
+    console.error(`Proxy error: ${error}`);
+    return ctx.text("Proxy error", 500);
+  }
+});
 
 app.use("*", async (ctx, next) => {
   const url = new URL(ctx.req.url);
@@ -28,13 +55,9 @@ app.use(
   }),
 );
 
-app.notFound((c) => {
-  try {
-    const html = readFileSync("./static/404.html", "utf-8");
-    return c.html(html, 404);
-  } catch {
-    return c.text("404 Not Found", 404);
-  }
+app.notFound((ctx) => {
+  const html = readFileSync("./static/404.html", "utf-8");
+  return ctx.html(html, 404);
 });
 
 serve(app, (info) => {
