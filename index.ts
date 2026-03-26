0e2087bfada3c690d27ba027cda1a79356dd1ec6
glebbash <glebbash@gmail.com>
2026-03-26 19:04:38 +0000

    chore: drop proxy endpoint due to deno deploy guidelines

--- index.ts
+++ index.ts
@@ -9,33 +9,6 @@ const foldersWithIndexHtml = readdirSync("static", { withFileTypes: true })
 
 const app = new Hono();
 
-app.all("/proxy/*", async (ctx) => {
-  const targetUrl = ctx.req.path.replace(/^\/proxy\//, "");
-  if (!targetUrl) {
-    return ctx.text("Target URL is required", 400);
-  }
-
-  try {
-    console.log(`Proxying request to: [${ctx.req.method}] ${targetUrl}`);
-
-    const response = await fetch(targetUrl, {
-      method: ctx.req.method,
-      headers: ctx.req.header(),
-      body: ["GET", "HEAD"].includes(ctx.req.method)
-        ? undefined
-        : await ctx.req.arrayBuffer(),
-    });
-
-    return new Response(response.body, {
-      status: response.status,
-      headers: response.headers,
-    });
-  } catch (error) {
-    console.error(`Proxy error: ${error}`);
-    return ctx.text("Proxy error", 500);
-  }
-});
-
 app.use("*", async (ctx, next) => {
   const url = new URL(ctx.req.url);
   const name = url.pathname.replace(/^\/|\/$/g, "");
